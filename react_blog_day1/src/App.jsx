fd3d0b68a9fac1e5f6de1b9060d9b50254dd835a
Indrajit Rananavare <indrajit6479@gmail.com>
2025-03-22 19:19:00 +0530

    *One component using in another component in same .jsx file

--- react_blog_day1/src/App.jsx
+++ react_blog_day1/src/App.jsx
@@ -1,13 +1,26 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <h1>Welcome to first react project</h1>
-      <h2>Hello World!!!</h2>
-      <h3>Hello React 19</h3>
-    </>
-  )
+    return (
+        <>
+            <Welcome/>
+            <h2>Hello World!!!</h2>
+            <h3>Hello React {sum()}</h3>
+        </>
+    )
+}
+
+function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to first react project</h1>
+            <h1>Namaste!!!</h1>
+        </div>
+    )
+}
+
+function sum() {
+    return 10 + 9;
 }
 
 export default App
